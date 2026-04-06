4ee54424f0f44d2d7d2744398c413e078c1570f6
Denis-ctr <denis.3131a@gmail.com>
2026-04-06 04:01:43 +0400

    New int lib

---
+++ src/include/typint.h
@@ -0,0 +1,23 @@
+#ifndef TYPINT_H
+#define TYPINT_H
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
+
+typedef char intfast8_t;
+typedef unsigned char uintfast8_t;
+typedef int int_fast16_t;
+typedef unsigned int uint_fast16_t;
+typedef int int_fast32_t;
+typedef unsigned int uint_fast32_t;
+typedef long long int_fast64_t;
+typedef unsigned long long uint_fast64_t;
+
+
+#endif
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,4 +1,5 @@
 #include "../include/vga.h"
+#include "../include/typint.h"
 
 void kernel_main() {
     vga_write();
