d2c6aae398a57b60f433d9f556fae465f03212d0
Denis-ctr <denis.3131a@gmail.com>
2026-04-08 22:34:09 +0400

    VGA header funksiyaları əlavə edildi

--- src/include/vga.h
+++ src/include/vga.h
@@ -1,6 +1,19 @@
 #ifndef VGA_H
 #define VGA_H
 
+#include "typint.h"
+
+// hardware terefden input output
+uint8_t byte_i(uint16_t port);
+void byte_o(uint16_t port, uint8_t data);
+
+// kursorun hereketi
+void cursor_set(int offset);
+int cursor_get();
+
+// memorye bit yazdirmaq
+void set_char_in_memory(char character, int32_t offset);
+
 void vga_write();
 
 #endif
